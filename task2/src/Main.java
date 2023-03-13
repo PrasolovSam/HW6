dda5cd4147e9a3cd871f38a5958480a25ac289d0
Roman Prasolov <rvprasolov@me.com>
2023-03-13 15:23:12 +0400

    Задача решена

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -13,22 +13,31 @@ public class Main {
         System.out.println("Введите третий вес: ");
         double third = new Scanner(System.in).nextDouble();
 
-        double greatest = 0;
-        double average = 0;
-        double smallest = 0;
-        //todo дописать логику программы ниже.
+        double greatest = 0.0;
+        double average = 0.0;
+        double smallest = 0.0;
+
+        if(first > second && first > third) {
+            greatest += first;
+        } else if (first < second && first < third) {
+            smallest += first;
+        } else average += first;
+
+        if(second > first && second > third) {
+            greatest += second;
+        } else if (second < first && second < third) {
+            smallest += second;
+        } else average += second;
+
+        if(third > first && third > second) {
+            greatest += third;
+        } else if (third < first && third < second) {
+            smallest += third;
+        } else average += third;
 
 
-
-
-
-
-
-        //todo
-
-
-        System.out.println("Наибольший вес: " + 10);
-        System.out.println("Средний вес: " + 9);
-        System.out.println("Наименьший вес: " + 1);
+        System.out.println("Greatest is " + greatest);
+        System.out.println("Average is " + average);
+        System.out.println("Smallest is " + smallest);
     }
 }
